6e1b75faa5f973f83bd192fe2a2e1d89d42be41b
Michael W. <44006905+Molodos@users.noreply.github.com>
2024-10-16 19:36:10 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,29 +1,11 @@
-import machine
 from pn532 import PN532Uart
-import utime
-
-# This example initializes the PN532 and then enters a forever loop
-# waiting for rfid tags to be read.
-#
-# Pinout:
-#      esp32 tx = 22 = pn532 rx
-#      esp32 rx = 23 = pn532 tx
-#
-#      esp32 OUT = 21 = buzzer (or led)
-#
-
-DEBUG = False
 
 
 def test():
-    buzzer = machine.Pin(21, machine.Pin.OUT)
-    buzzer.off()
 
-    # NOTE: on several of the esp32-wrover dev boards, the default uart2 pins
-    #       conflict with the psRam so the pn532 is plugged into two unused
-    #       pins instead.
+    # Version for RP2040 Zero
     try:
-        rf = PN532Uart(2, tx=22, rx=23, debug=DEBUG)
+        rf = PN532Uart(1, tx=4, rx=5, debug=false)
         rf.SAM_configuration()
         ic, ver, rev, support = rf.get_firmware_version()
         print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
@@ -35,9 +17,6 @@ def test():
         try:
             uid = rf.read_passive_target()
             print("Card UUID: ", [hex(i) for i in uid])
-            buzzer.on()
-            utime.sleep_ms(200)
-            buzzer.off()
         except Exception as e:
             # NOTE: This is important to stop the reader from reporting cards after
             #       we are no longer waiting.
